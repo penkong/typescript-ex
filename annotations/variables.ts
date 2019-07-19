d5f523e8f7b9e50fc811be8762d5d7b35847926f
mazdak <nazemi.works@gmail.com>
2019-07-18 23:07:21 -0700

    some annotations2

---
+++ annotations/variables.ts
@@ -0,0 +1,32 @@
+// this is annotations
+let apples: number = 5;
+
+let speed: string = "fast";
+
+let hasName: boolean = true;
+
+let nothing: null = null;
+
+let nothings: undefined = undefined;
+
+// built in objs
+
+let now: Date = new Date();
+
+// Array
+
+let colors: string[] = ["red", "blue", "green"];
+let myNums: number[] = [1, 3, 2, 4];
+let truth: boolean[] = [true, false];
+
+// classes
+
+class Car {}
+let car: Car = new Car();
+
+// obj literal
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
--- fetchwithts/index.ts
+++ fetchwithts/index.ts
@@ -5,6 +5,8 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+// types = methods + props like string or num;
+// Todo is also type
 // interfaces in ts use to define the structure of obj
 interface Todo {
   id: number;
@@ -14,12 +16,17 @@ interface Todo {
 
 axios.get(url).then(res => {
   const todo = res.data as Todo;
-  const ID = todo.id;
+  const id = todo.id;
   const title = todo.title;
-  const finished = todo.completed;
-  console.log(`
-    the todo with id: ${ID}
-    has title of: ${title}
-    is it finished? ${finished}
-  `);
+  const completed = todo.completed;
+  logTodo(id, title, completed);
 });
+
+// type notation => help to specify correct params on call
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    the todo with id: ${id}
+    has title of: ${title}
+    is it finished? ${completed}
+  `);
+};
