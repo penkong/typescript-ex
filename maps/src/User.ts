6e73d6d86f4fdcd4801677d7e75d8c5c026f292d
mazdak <nazemi.works@gmail.com>
2019-07-19 06:19:43 -0700

    make structure of classes and use typed definitely for faker

---
+++ maps/src/User.ts
@@ -0,0 +1,19 @@
+// use type definition file  as adaptor between ts files and js libs
+// those created by Definitely Typed project
+import faker from "faker";
+
+class User {
+  name: string;
+  // it will be a obj or string but we need init that in below
+  location: {
+    lat: number;
+    lng: number;
+  };
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
+  }
+}
