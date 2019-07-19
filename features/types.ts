fd0a92f56b32c6e18b1b433a43167a5e11ce9012
mazdak <nazemi.works@gmail.com>
2019-07-18 23:06:48 -0700

    some annotations

---
+++ features/types.ts
@@ -0,0 +1,13 @@
+const today = new Date();
+today.getMonth();
+
+const person = {
+  age: 20
+};
+
+class Color {}
+
+const red = new Color();
+
+// type annotations we add code
+// type inference ts try and do it
