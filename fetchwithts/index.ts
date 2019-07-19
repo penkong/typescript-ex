d41c86aa9f4e873d1d26f3c28d792b33bc3c5d53
mazdak <nazemi.works@gmail.com>
2019-07-18 20:49:37 -0700

    catch er in dev interfacc and as Sth

--- fetchwithts/index.ts
+++ fetchwithts/index.ts
@@ -5,6 +5,21 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+// interfaces in ts use to define the structure of obj
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(res => {
-  console.log(res.data);
+  const todo = res.data as Todo;
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+  console.log(`
+    the todo with id: ${ID}
+    has title of: ${title}
+    is it finished? ${finished}
+  `);
 });
