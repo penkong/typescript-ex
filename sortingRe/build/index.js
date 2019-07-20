9588c77712cc4b9fe127d731b6f4f63bb2a1e09f
mazdak <nazemi.works@gmail.com>
2019-07-20 01:14:27 -0700

    sorter app starter

--- sortingRe/build/index.js
+++ sortingRe/build/index.js
@@ -1,4 +1,20 @@
 "use strict";
 // tsc --init
 // tsc -w
-console.log('helollol');
+// add nodemon and concurrently with tsconfig.json
+// class Sorter {
+//   collection: number[];
+//   constructor(collection: number[]) {
+//     this.collection = collection;
+//   }
+// }
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () { };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, 5, 7]);
+sorter.sort();
+console.log(sorter.collection);
--- sortingRe/src/index.ts
+++ sortingRe/src/index.ts
@@ -1,3 +1,19 @@
 // tsc --init
 // tsc -w
-console.log('helollol');
+// add nodemon and concurrently with tsconfig.json
+
+// class Sorter {
+//   collection: number[];
+
+//   constructor(collection: number[]) {
+//     this.collection = collection;
+//   }
+// }
+class Sorter {
+  constructor(public collection: number[]) {}
+  sort(): void {}
+}
+
+const sorter = new Sorter([10, 3, 5, 7]);
+sorter.sort();
+console.log(sorter.collection);
