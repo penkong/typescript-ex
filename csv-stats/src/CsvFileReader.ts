f9ac20bb4e3d5cdf5dfc285fb68547ea790bd12e
mazdak <nazemi.works@gmail.com>
2019-07-20 21:30:26 -0700

    csv reader class and also enum in ts

---
+++ csv-stats/src/CsvFileReader.ts
@@ -0,0 +1,23 @@
+//
+import fs from 'fs';
+
+export class CsvFileReader {
+  data: string[][] = [];
+  constructor(public fileName: string) {}
+
+  read(): void {
+    this.data = fs
+      .readFileSync(this.fileName, {
+        // we tell what type of file we expect find in csv files.
+        // if not define this it will turn back to us a buffer
+        encoding: 'utf-8'
+      })
+      // how to parse it.
+      .split('\n')
+      .map(
+        (row: string): string[] => {
+          return row.split(',');
+        }
+      );
+  }
+}
--- csv-stats/src/index.ts
+++ csv-stats/src/index.ts
@@ -1,2 +1,32 @@
 // csv => load it => node => parse => analyze  => stats
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+// analyze
+// instead of obj in ts we use enum - enumeration to show simple data
+// enum - to store some related close values
+// enum is signaling to others this is collections or related values
+// it is also a type by itself
+// behind scene it creates obj
+// we use enum for definite set of data not categories of movie or any async req
+// not for large set of values;
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+// report
+console.log(`man united won ${manUnitedWins} games`);
