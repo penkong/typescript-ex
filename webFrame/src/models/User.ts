f9d4c7168462feaae501df71520ec8ecfade1da2
mazdak <nazemi.works@gmail.com>
2019-07-22 06:08:01 -0700

    make user class done step before refactor

--- webFrame/src/models/User.ts
+++ webFrame/src/models/User.ts
@@ -1,7 +1,10 @@
+import axios, { AxiosResponse } from 'axios';
+
 //
 interface UserProps {
   name?: string;
   age?: number;
+  id?: number;
 }
 
 type Callback = () => void;
@@ -39,5 +42,21 @@ export class User {
   // "users" => /users "info" => /info
   // fetcher
   // saver
-  fetch() {}
+  fetch(): void {
+    axios.get(`http://localhost:3000/users/${this.get('id')}`).then(
+      (res: AxiosResponse): void => {
+        this.set(res.data);
+      }
+    );
+  }
+  save(): void {
+    const id = this.get('id');
+    // post without id put need id
+    if (id) {
+      // put
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post('http://localhost:3000/users', this.data);
+    }
+  }
 }
