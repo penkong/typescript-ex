74dfd0e851d901bcadfe88edcafbaa25339c2e7e
mazdak <nazemi.works@gmail.com>
2019-07-22 06:28:38 -0700

    extract events from main user class

---
+++ webFrame/src/models/Eventing.ts
@@ -0,0 +1,23 @@
+type Callback = () => void;
+
+export class Eventing {
+  // type annotation
+  events: { [key: string]: Callback[] } = {};
+
+  // adding eventing for framework
+  // equal as addEventListener
+  // register an event
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+  // trigger (call) an event or call it
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) return;
+    handlers.forEach(callback => {
+      callback();
+    });
+  }
+}
--- webFrame/src/models/User.ts
+++ webFrame/src/models/User.ts
@@ -7,12 +7,7 @@ interface UserProps {
   id?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-  // type annotation
-  events: { [key: string]: Callback[] } = {};
-
   constructor(private data: UserProps) {}
   get(propName: string): number | string {
     return this.data[propName];
@@ -21,22 +16,7 @@ export class User {
   set(update: UserProps): void {
     Object.assign(this.data, update);
   }
-  // adding eventing for framework
-  // equal as addEventListener
-  // register an event
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-  // trigger (call) an event or call it
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-    if (!handlers || handlers.length === 0) return;
-    handlers.forEach(callback => {
-      callback();
-    });
-  }
+
   // promises to save data and user info to backend or whatever
   // json-server -w db.json => each field on db is like route for backend
   // "users" => /users "info" => /info
